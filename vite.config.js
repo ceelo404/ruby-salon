05fef4f06127a857f4c177c34e598304a3c7d326
ceelo404 <ceelo404@gmail.com>
2025-08-15 22:28:05 -0400

    vite image optimizer

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer'; // <-- 1. IMPORT THE PLUGIN
+
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    ViteImageOptimizer({
+      // Example configuration for PNG files
+      png: {
+        quality: 80, // Compresses PNGs to 80% of their original quality
+      },
+    }),
+  ],
+});
